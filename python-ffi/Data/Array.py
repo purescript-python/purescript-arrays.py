74fde55da3fdb38c5443b4b0586429def5e8925f
Xiang <hx.hongxiang@gmail.com>
2020-03-21 22:32:04 +0800

    feature(): add Array.py functions

--- python-ffi/Data/Array.py
+++ python-ffi/Data/Array.py
@@ -1,4 +1,6 @@
-_buitins = {"range": range}
+import functools
+
+_buitins = {"range": range, "filter": filter}
 
 globals()["range"] = lambda start: lambda end: list(_buitins["range"](start, end))
 
@@ -40,3 +42,123 @@ def _mkFromFoldableImpl():
 
 
 fromFoldableImpl = _mkFromFoldableImpl()
+
+length = lambda xs: len(xs)
+
+cons = lambda e: lambda l: [e, *l]
+
+sonc = lambda xs: lambda x: [*xs, x]
+
+
+def unconsImpl(empty):
+    def next_f(next_):
+        def xs_(xs):
+            if len(xs) == 0:
+                return empty(None)
+            else:
+                return next_(xs[0])(xs[1:])
+
+        return xs_
+
+    return next_f
+
+
+def _indexImpl(just, nothing, xs, i):
+    if i < 0 or i >= len(xs):
+        return nothing
+    else:
+        return just(xs[i])
+
+
+indexImpl = lambda just: lambda nothing: lambda xs: lambda i: _indexImpl(
+    just, nothing, xs, i
+)
+
+
+def _findIndexImpl(just, nothing, f, xs):
+    for i, x in enumerate(xs):
+        if f(x):
+            return just(i)
+    return nothing
+
+
+findIndexImpl = lambda just: lambda nothing: lambda f: lambda xs: _findIndexImpl(
+    just, nothing, f, xs
+)
+
+
+def _insertAtImpl(just, nothing, i, a, l):
+    if i < 0 or i >= len(l):
+        return nothing
+    ll = l.copy()
+    ll.insert(i, a)
+    return just(ll)
+
+
+_insertAt = lambda just: lambda nothing: lambda i: lambda a: lambda l: _insertAtImpl(
+    just, nothing, i, a, l
+)
+
+
+def _deleteAtImpl(just, nothing, i, a, l):
+    if i < 0 or i >= len(l):
+        return nothing
+    return just(l[:i] + l[i + 1 :])
+
+
+_deletetAt = lambda just: lambda nothing: lambda i: lambda a: lambda l: _insertAtImpl(
+    just, nothing, i, a, l
+)
+
+
+def _updateAtImpl(just, nothing, i, a, l):
+    if i < 0 or i >= len(l):
+        return nothing
+    ll = l.copy()
+    ll[i] = a
+    return just(ll)
+
+
+_updateAt = lambda just: lambda nothing: lambda i: lambda a: lambda l: _insertAtImpl(
+    just, nothing, i, a, l
+)
+
+reverse = lambda xs: reversed(xs)
+
+
+def concat(xss):
+    result = []
+    for x in xss:
+        result.extend(x)
+    return result
+
+
+globals()["filter"] = lambda f: lambda xs: list(_buitins["filter"](f, xs))
+
+
+def partition(f):
+    def result(xs):
+        yes = []
+        no = []
+        for x in xs:
+            if f(x):
+                yes.append(x)
+            else:
+                no.append(x)
+        return {"yes": yes, "no": no}
+
+    return result
+
+
+sortImpl = lambda f: lambda xs: sorted(
+    xs, key=functools.cmp_to_key(lambda a, b: f(a)(b))
+)
+
+slice = lambda s: lambda e: lambda xs: xs[s:e]
+
+take = lambda n: lambda xs: xs[:n]
+drop = lambda n: lambda xs: xs[n:]
+
+zipWith = lambda f: lambda xs: lambda ys: list(map(f, zip(xs, ys)))
+
+unsafeIndexImpl = lambda xs: lambda n: xs[n]
